9a35611c8b2b7af79b1c0acae2d5eb7b2401784d
Misite Bao <i@misitebao.com>
2022-05-30 19:37:51 +0800

    feat(nuget): add c# project code

---
+++ nuget/misitebao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace main
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      String path = "https://misitebao.com";
+
+      Process p = new Process();
+
+      try
+      {
+        p.StartInfo.UseShellExecute = true;
+        p.StartInfo.FileName = path;
+        p.Start();
+      }
+      catch (Exception e)
+      {
+        throw new Exception(string.Format("An error occurred when opening '{0}': {1}", path, e));
+      }
+
+      Console.WriteLine(string.Format("Opened '{0}' successfully.", path));
+    }
+  }
+}
+
+
+
+
+
+
+
+
