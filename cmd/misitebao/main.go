a5fbc2773454b83eb7ff16e01027297c46bc5ceb
Misitebao <i@misitebao.com>
2022-06-01 18:30:59 +0800

    release cli v1.0.0 (#7)

---
+++ cmd/misitebao/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/pkg/browser"
+)
+
+func main() {
+	browser.OpenURL("https://misitebao.com")
+}
---
+++ npm/misitebao/bin/index.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+
+const open = require('open');
+
+open("https://misitebao.com");
