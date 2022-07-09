a8fba11ca6302919fcccd0e541137cd22f47f2aa
misitebao <i@misitebao.com>
2022-07-09 22:56:19 +0800

    feat: add error handling

--- cmd/misitebao/main.go
+++ cmd/misitebao/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pkg/browser"
 )
 
 func main() {
-	browser.OpenURL("https://misitebao.com")
+	var path = "https://misitebao.com"
+
+	err := browser.OpenURL(path)
+	if err != nil {
+		panic(fmt.Sprintf("An error occurred when opening '%s': %s", path, err))
+	}
+
+	fmt.Printf("Opened '%s' successfully.", path)
 }
--- npm/misitebao/bin/index.js
+++ npm/misitebao/bin/index.js
@@ -1,5 +1,15 @@
 #! /usr/bin/env node
 
-const open = require('open');
+const open = require("open");
 
-open("https://misitebao.com");
+const path = "https://misitebao.com";
+
+(function main() {
+  open(path)
+    .then((res) => {
+      console.log(`Opened '${path}' successfully.`);
+    })
+    .catch((err) => {
+      console.log(`An error occurred when opening '${path}': ${err}`);
+    });
+})();
