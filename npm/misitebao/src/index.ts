8c5237e1430c9885a6c21a425c0b5678241765cc
Misite Bao <i@misitebao.com>
2022-08-25 19:47:46 +0800

    feat(npm): add variable type

--- npm/misitebao/src/index.ts
+++ npm/misitebao/src/index.ts
@@ -1,4 +1,4 @@
 // Module misitebao exposes constant information
 
-export const FULLNAME = "Misite Bao";
-export const EMAIL = "i@misitebao.com";
+export const FULLNAME: string = "Misite Bao";
+export const EMAIL: string = "i@misitebao.com";
