79ba54dc64c4fa12243f67f105abce1607f5ebfc
Misite Bao <i@misitebao.com>
2023-01-17 20:41:54 +0800

    release(pypi): v1.0.2

--- pypi/misitebao/setup.py
+++ pypi/misitebao/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='misitebao',
-    version='1.0.1',
+    version='1.0.2',
     keywords=["misitebao", "misi"],
     license="MIT License",
     author='Misite Bao',
